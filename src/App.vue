e633f78b0476f759fc74c17dce8c18078d52e167
Zac Allen <zac.allen@outlook.com>
2021-07-19 09:35:50 +0200

    added components for friends list

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+<section>
+    <header>
+        <h1>My Friends</h1>
+    </header>    
+    <ul>
+        <friend-contact></friend-contact>
+        <friend-contact></friend-contact>
+    </ul>
+</section>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            friends: [
+                {
+                    id: "manuel",
+                    name: "Manuel Lorenz",
+                    phone: "0123 45689 90",
+                    email: "manuel@localhost.com"
+                },
+                {
+                    id: "julie",
+                    name: "Julie Jones",
+                    phone: "0987 12345 78",
+                    email: "julie@localhost.com"
+                }
+            ]
+        }
+    } 
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Jost&display=swap');
+</style>
